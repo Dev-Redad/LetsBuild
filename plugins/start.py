f69219599d624e392b75d2953f42c8d8e26aacac
Shubhamdev087 <neel79571@gmail.com>
2025-07-15 11:41:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,5 +1,4 @@
-# Updated start.py with Razorpay integration, ad token fully removed
-
+# Updated start.py with Razorpay payment integration and ad-token removed
 import asyncio
 import base64
 import logging
@@ -9,6 +8,7 @@ import re
 import string
 import time
 
+import razorpay
 from pyrogram import Client, filters
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
@@ -31,201 +31,130 @@ from helper_func import (
     get_messages,
 )
 from database.database import add_user, del_user, full_userbase, present_user
-from razorpay_integration import create_payment_link, is_payment_completed  # make sure you have this
+
+RAZORPAY_KEY_ID = "rzp_live_Kfvz8iobE8iUZc"
+RAZORPAY_KEY_SECRET = "bcPhJQ2pHTaaF94FhWCEl6eD"
+razorpay_client = razorpay.Client(auth=(RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET))
+
+user_orders = {}
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
+    if not await present_user(id):
+        try:
+            await add_user(id)
+        except:
+            pass
 
-    if id == ADMINS:
-        await message.reply("You are the owner! Additional actions can be added here.")
-    else:
-        if not await present_user(id):
-            try:
-                await add_user(id)
-            except:
-                pass
-
-        if len(message.text) > 7:
-            try:
-                base64_string = message.text.split(" ", 1)[1]
-            except:
-                return
-
-            _string = await decode(base64_string)
-            argument = _string.split("-")
-
-            if len(argument) == 3:
-                try:
-                    start = int(int(argument[1]) / abs(client.db_channel.id))
-                    end = int(int(argument[2]) / abs(client.db_channel.id))
-                except:
-                    return
-                ids = list(range(start, end + 1)) if start <= end else list(range(start, end - 1, -1))
-            elif len(argument) == 2:
-                try:
-                    ids = [int(int(argument[1]) / abs(client.db_channel.id))]
-                except:
-                    return
-
-            # Check payment status
-            payment_completed = await is_payment_completed(user_id=id)
-            if not payment_completed:
-                pay_link, qr_img = await create_payment_link(user_id=id, amount=2)
-                keyboard = InlineKeyboardMarkup([
-                    [InlineKeyboardButton("I've Paid ✅", callback_data=f"check_payment_{id}")]
-                ])
-                await message.reply_photo(
-                    photo=qr_img,
-                    caption=f"Please pay ₹2 to continue.\nPayment Link: {pay_link}",
-                    reply_markup=keyboard
-                )
-                return
-
-            temp_msg = await message.reply("Please wait...")
-            try:
-                messages = await get_messages(client, ids)
-            except:
-                await message.reply_text("Something went wrong..!")
-                return
-            await temp_msg.delete()
-
-            for msg in messages:
-                caption = (
-                    CUSTOM_CAPTION.format(
-                        previouscaption="" if not msg.caption else msg.caption.html,
-                        filename=msg.document.file_name
-                    ) if bool(CUSTOM_CAPTION) and bool(msg.document) else
-                    "" if not msg.caption else msg.caption.html
-                )
-
-                reply_markup = msg.reply_markup if DISABLE_CHANNEL_BUTTON else None
-
-                try:
-                    await msg.copy(
-                        chat_id=message.from_user.id,
-                        caption=caption,
-                        parse_mode=ParseMode.HTML,
-                        reply_markup=reply_markup,
-                        protect_content=PROTECT_CONTENT
-                    )
-                    await asyncio.sleep(0.5)
-                except FloodWait as e:
-                    await asyncio.sleep(e.x)
-                    await msg.copy(
-                        chat_id=message.from_user.id,
-                        caption=caption,
-                        parse_mode=ParseMode.HTML,
-                        reply_markup=reply_markup,
-                        protect_content=PROTECT_CONTENT
-                    )
-                except:
-                    pass
+    if "pay_" in message.text:
+        _, order_id, encoded = message.text.split("_")
+        payment = razorpay_client.order.fetch(order_id)
+        if payment['status'] == 'paid':
+            base64_string = encoded
+            await send_files(client, message, base64_string)
         else:
-            reply_markup = InlineKeyboardMarkup(
-                [[
-                    InlineKeyboardButton("About Me", callback_data="about"),
-                    InlineKeyboardButton("Close", callback_data="close")
-                ]]
-            )
-            await message.reply_text(
-                text=START_MSG.format(
-                    first=message.from_user.first_name,
-                    last=message.from_user.last_name,
-                    username=None if not message.from_user.username else '@' + message.from_user.username,
-                    mention=message.from_user.mention,
-                    id=message.from_user.id
-                ),
-                reply_markup=reply_markup,
-                disable_web_page_preview=True,
-                quote=True
-            )
+            await message.reply("Payment not completed. Please pay and click 'I've Paid' again.")
+        return
 
+    elif len(message.text) > 7:
+        try:
+            base64_string = message.text.split(" ", 1)[1]
+        except:
+            return
 
-@Bot.on_callback_query(filters.regex("^check_payment_"))
-async def check_payment_status(client: Client, callback_query: CallbackQuery):
-    user_id = int(callback_query.data.split("_")[2])
-    if await is_payment_completed(user_id):
-        await callback_query.message.edit_caption("✅ Payment received. Please press /start again.")
+        order = razorpay_client.order.create({
+            "amount": 200,  # 2 INR in paise
+            "currency": "INR",
+            "payment_capture": 1
+        })
+
+        payment_url = f"https://rzp.io/l/{order['id']}"
+        user_orders[id] = {"order_id": order['id'], "encoded": base64_string}
+
+        await message.reply(
+            f"To access this file, please pay ₹2.",
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("Pay ₹2", url=payment_url)],
+                [InlineKeyboardButton("I've Paid", callback_data=f"checkpay_{order['id']}")]
+            ])
+        )
     else:
-        await callback_query.answer("❌ Payment not received yet. Please wait or retry.", show_alert=True)
-
-
-@Bot.on_message(filters.command('start') & filters.private)
-async def not_joined(client: Client, message: Message):
-    buttons = [
-        [
-            InlineKeyboardButton("Join Channel", url=client.invitelink),
-            InlineKeyboardButton("Join Channel", url=client.invitelink2),
-        ]
-    ]
-    try:
-        buttons.append([
-            InlineKeyboardButton("Try Again", url=f"https://t.me/{client.username}?start={message.command[1]}")
+        reply_markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton("About Me", callback_data="about"),
+             InlineKeyboardButton("Close", callback_data="close")]
         ])
-    except IndexError:
-        pass
+        await message.reply_text(
+            text=START_MSG.format(
+                first=message.from_user.first_name,
+                last=message.from_user.last_name,
+                username=None if not message.from_user.username else '@' + message.from_user.username,
+                mention=message.from_user.mention,
+                id=message.from_user.id
+            ),
+            reply_markup=reply_markup,
+            disable_web_page_preview=True,
+            quote=True
+        )
 
-    await message.reply(
-        text=FORCE_MSG.format(
-            first=message.from_user.first_name,
-            last=message.from_user.last_name,
-            username=None if not message.from_user.username else '@' + message.from_user.username,
-            mention=message.from_user.mention,
-            id=message.from_user.id
-        ),
-        reply_markup=InlineKeyboardMarkup(buttons),
-        quote=True,
-        disable_web_page_preview=True
-    )
+@Bot.on_callback_query(filters.regex("checkpay_"))
+async def check_payment(bot: Client, query: CallbackQuery):
+    user_id = query.from_user.id
+    order_id = query.data.split("checkpay_")[1]
+
+    try:
+        payment = razorpay_client.order.fetch(order_id)
+        if payment['status'] == 'paid':
+            encoded = user_orders[user_id]['encoded']
+            await send_files(bot, query.message, encoded)
+            await query.message.delete()
+        else:
+            await query.answer("Payment not completed yet.", show_alert=True)
+    except Exception as e:
+        logging.error(str(e))
+        await query.answer("Could not verify payment.", show_alert=True)
 
 
-@Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
-async def get_users(client: Bot, message: Message):
-    msg = await client.send_message(chat_id=message.chat.id, text="<b>Processing ...</b>")
-    users = await full_userbase()
-    await msg.edit(f"{len(users)} users are using this bot")
+async def send_files(client: Client, message: Message, base64_string: str):
+    try:
+        _string = await decode(base64_string)
+        argument = _string.split("-")
 
+        if len(argument) == 3:
+            start = int(int(argument[1]) / abs(client.db_channel.id))
+            end = int(int(argument[2]) / abs(client.db_channel.id))
+            ids = list(range(start, end + 1)) if start <= end else list(range(start, end - 1, -1))
+        else:
+            ids = [int(int(argument[1]) / abs(client.db_channel.id))]
 
-@Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
-async def send_text(client: Bot, message: Message):
-    if message.reply_to_message:
-        query = await full_userbase()
-        broadcast_msg = message.reply_to_message
-        total = successful = blocked = deleted = unsuccessful = 0
+        temp_msg = await message.reply("Please wait...")
+        messages = await get_messages(client, ids)
+        await temp_msg.delete()
 
-        pls_wait = await message.reply("<i>Broadcasting Message.. This will take some time</i>")
-        for chat_id in query:
+        for msg in messages:
+            caption = (
+                CUSTOM_CAPTION.format(
+                    previouscaption="" if not msg.caption else msg.caption.html,
+                    filename=msg.document.file_name
+                ) if bool(CUSTOM_CAPTION) and bool(msg.document) else
+                "" if not msg.caption else msg.caption.html
+            )
+
+            reply_markup = msg.reply_markup if DISABLE_CHANNEL_BUTTON else None
+
             try:
-                await broadcast_msg.copy(chat_id)
-                successful += 1
+                await msg.copy(
+                    chat_id=message.chat.id,
+                    caption=caption,
+                    parse_mode=ParseMode.HTML,
+                    reply_markup=reply_markup,
+                    protect_content=PROTECT_CONTENT
+                )
+                await asyncio.sleep(0.5)
             except FloodWait as e:
                 await asyncio.sleep(e.x)
-                await broadcast_msg.copy(chat_id)
-                successful += 1
-            except UserIsBlocked:
-                await del_user(chat_id)
-                blocked += 1
-            except InputUserDeactivated:
-                await del_user(chat_id)
-                deleted += 1
             except:
-                unsuccessful += 1
-            total += 1
-
-        status = (
-            f"<b><u>Broadcast Completed</u>\n\n"
-            f"Total Users: <code>{total}</code>\n"
-            f"Successful: <code>{successful}</code>\n"
-            f"Blocked Users: <code>{blocked}</code>\n"
-            f"Deleted Accounts: <code>{deleted}</code>\n"
-            f"Unsuccessful: <code>{unsuccessful}</code></b>"
-        )
-
-        return await pls_wait.edit(status)
-    else:
-        msg = await message.reply(
-            "<code>Use this command as a reply to any Telegram message without any spaces.</code>"
-        )
-        await asyncio.sleep(8)
-        await msg.delete()
+                pass
+    except Exception as e:
+        logging.error(str(e))
+        await message.reply_text("Something went wrong while sending files.")
